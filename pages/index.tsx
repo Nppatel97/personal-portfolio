16fb014773c136f88668f855e1bef18039a45799
Nisarg Patel <np96380@gmail.com>
2022-11-02 23:31:30 +0530

    skills added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import About from "../components/About";
 import Experience from "../components/Experience";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 export default function Home() {
   return (
@@ -26,7 +28,14 @@ export default function Home() {
       </section>
 
       {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
+
       {/* Skills */}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
       {/* About */}
       <section id="about" className="snap-start">
