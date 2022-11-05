a0b19ed4992f65f4ed8be36451af698ed72f0318
Nisarg Patel <np96380@gmail.com>
2022-11-05 14:48:34 +0530

    update to Sanity

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -7,6 +7,9 @@ import schemaTypes from "all:part:@sanity/base/schema-type";
 // We import object and document schemas
 import pageInfo from "./pageInfo";
 import experience from "./experience";
+import skill from "./skill";
+import social from "./social";
+import project from "./project";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -19,5 +22,8 @@ export default createSchema({
     // in the studio.
     pageInfo,
     experience,
+    skill,
+    social,
+    project,
   ]),
 });
---
+++ sanity/schemas/social.js
@@ -0,0 +1,18 @@
+export default {
+  name: "social",
+  title: "Social",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: "Title of the Social Media Platform",
+      type: "string",
+    },
+    {
+      name: "url",
+      title: "URL",
+      type: "url",
+    },
+  ],
+};
