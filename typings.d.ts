b4308bb14771e13b40c31d0b86301c2822a6064b
Nisarg Patel <np96380@gmail.com>
2022-11-05 16:11:06 +0530

    type definitions added

---
+++ typings.d.ts
@@ -0,0 +1,66 @@
+interface SanityBody {
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _updatedAt: string;
+}
+
+interface Image {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+}
+
+export interface PageInfo extends SanityBody {
+  _type: "pageInfo";
+  address: string;
+  backgroundInfo: string;
+  cellNumber: string;
+  email: string;
+  mainImg: Image;
+  name: string;
+  profileImg: Image;
+  role: string;
+}
+
+export interface Technology extends SanityBody {
+  _type: "skill";
+  title: string;
+  image: Image;
+  progress: number;
+}
+
+export interface Experience extends SanityBody {
+  _type: "experience";
+  company: string;
+  currentlyWorkingHere: boolean;
+  dateEnded: date;
+  dateStarted: date;
+  descPoints: string[];
+  jobTitle: string;
+  technologies: Technology[];
+}
+
+export interface Skill extends SanityBody {
+  _type: "skill";
+  title: string;
+  image: Image;
+  progress: number;
+}
+
+export interface Project extends SanityBody {
+  _type: "project";
+  title: string;
+  image: Image;
+  linktoBuild: string;
+  summary: string;
+  technologies: Technology[];
+}
+
+export interface Social extends SanityBody {
+  _type: "social";
+  title: string;
+  url: string;
+}
