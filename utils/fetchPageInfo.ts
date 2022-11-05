9ed238261d80db2fe523da085575d7ca19283eb9
Nisarg Patel <np96380@gmail.com>
2022-11-05 19:49:12 +0530

    bug fix in links

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -3,7 +3,7 @@ import { PageInfo } from "../typings";
 const dev = process.env.NODE_ENV !== "production";
 export const server = dev
   ? "http://localhost:3000"
-  : "https://personal-portfolio-two-steel.vercel.app/";
+  : "https://personal-portfolio-two-steel.vercel.app";
 
 export const fetchPageInfo = async () => {
   const res = await fetch(`${server}/api/getPageInfo`);
