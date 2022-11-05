5bf5abccdfea2af46c7e15531016e94fb9ac2e52
Nisarg Patel <np96380@gmail.com>
2022-11-05 19:10:24 +0530

    custom Domain links added

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -3,7 +3,7 @@ import { Experience } from "../typings";
 const dev = process.env.NODE_ENV !== "production";
 export const server = dev
   ? "http://localhost:3000"
-  : "https://personal-portfolio-two-steel.vercel.app/";
+  : "https://nisargppatel.com";
 
 export const fetchExperiences = async () => {
   const res = await fetch(`${server}/api/getExperiences`);
--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -3,7 +3,7 @@ import { Social } from "../typings";
 const dev = process.env.NODE_ENV !== "production";
 export const server = dev
   ? "http://localhost:3000"
-  : "https://personal-portfolio-two-steel.vercel.app/";
+  : "https://nisargppatel.com";
 
 export const fetchSocials = async () => {
   const res = await fetch(`${server}/api/getSocials`);
